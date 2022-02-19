4626f730bea30317e08b269393e5d79d4f268f2a
Natalie Martin <nmartin2473@gmail.com>
2022-02-18 19:41:05 -0700

    Website is up!
    My new personal website is finally up! There's still a lot of 
    construction going on here, but I hope you enjoy your stay so far! Look 
    forward to lots of new pages coming soon.

---
+++ src/script/rss.js
@@ -0,0 +1,59 @@
+function buildPosts(text) {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(text, 'text/xml');
+  const items = doc.querySelectorAll('item');
+  document.getElementById("updates").innerHTML = "";
+  let i = 0;
+  for (const item of items) {
+    const title = item.querySelector("title")?.innerHTML;
+    const date = item.querySelector("pubDate")?.innerHTML;
+    const description = item.querySelector("description")?.innerHTML;
+
+    const feedItem = document.createElement("div");
+    feedItem.className = "feed-item";
+    const header = document.createElement("div");
+
+    const dateElement = document.createElement("time");
+    dateElement.className = "feed-date";
+    dateElement.setAttribute("datetime", date);
+    const humanDate = (new Date(date)).toLocaleDateString("ja-JP");
+    dateElement.innerText = humanDate + ": ";
+    header.appendChild(dateElement);
+
+    const titleElement = document.createElement("span");
+    titleElement.className = "feed-title";
+    titleElement.innerText = title;
+    header.appendChild(titleElement);
+
+    feedItem.appendChild(header);
+
+    if (description) {
+      const descriptionElement = document.createElement("div");
+      descriptionElement.className = "feed-desc";
+      if (markdown) {
+        descriptionElement.innerHTML = markdown.toHTML(description);
+      } else {
+        descriptionElement.innerHTML = description;
+      }
+      feedItem.appendChild(descriptionElement);
+    }
+
+    document.getElementById("updates").appendChild(feedItem);
+    if (i++ < items.length - 1) {
+      const hr = document.createElement("hr");
+      document.getElementById("updates").appendChild(hr);
+    }
+  }
+}
+
+function fetchRss() {
+  fetch("/rss.xml").then((response) => {
+    response.text().then((text) => {
+      buildPosts(text);
+    });
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetchRss();
+});
---
+++ src/script/twtxt.js
@@ -0,0 +1,80 @@
+function makeBlog(tweets) {
+  const blog = document.getElementById("microblog");
+  blog.innerHTML = '';
+  for (const tweet of tweets) {
+    const entry = document.createElement("div");
+    entry.id = `${tweet.date - 0}`;
+    entry.style = "border-bottom: 1px solid #000; padding: 1em;";
+    entry.className = "twtxt-tweet";
+
+    const date = document.createElement("time");
+    date.setAttribute("datetime", tweet.date.toISOString());
+    date.innerText = tweet.date.toLocaleString("ja-JA");
+    date.style = "font-size: 0.8em; font-style: italic; font-color: #333;"
+    entry.appendChild(date);
+
+    const content = document.createElement("div");
+    content.className = "twtxt-content";
+    content.innerHTML = tweet.text;
+    entry.appendChild(content);
+
+    blog.appendChild(entry);
+  }
+}
+
+function expandWordle(text) {
+  const matches = text.match(/(.+?)Wordle \d+ \d\/\d\*?/i)
+  if (matches && matches.length > 0) {
+    const wordleHeader = matches[0];
+    const wordleBody = text.substr(matches[0].length).trim();
+    const wordleTurns = wordleBody.split(/\s+\/?\s*/).join("<br />");
+    return `<p>${wordleHeader}<br />
+    <br />
+    ${wordleTurns}`;
+  }
+  return text;
+}
+
+function buildTweets(text) {
+  const lines = text.split("\n");
+  const tweets = [];
+  for (const line of lines) {
+    if (line.trim()[0] === '#' || line.trim().length === 0) {
+      continue;
+    }
+    const pieces = line.split("\t");
+    const date = new Date(pieces.shift());
+    if (isNaN(date.getDate())) {
+      continue;
+    }
+    const text = pieces.join("\t");
+    if (tweets.length > 0 && tweets[tweets.length - 1].date + 0 === date + 0) {
+      tweets[tweets.length - 1].text += "\n\n" + text;
+    } else {
+      tweets.push({
+        date: date,
+        text: text,
+      });
+    }
+  }
+  console.log(tweets);
+  tweets.sort((a, b) => b.date - a.date);
+  return tweets.map((tweet) => {
+    return {
+      date: tweet.date,
+      text: markdown ? expandWordle(markdown.toHTML(tweet.text)) : expandWordle(tweet.text),
+    }
+  });
+}
+
+function fetchBlog() {
+  fetch("/twtxt.txt").then((response) => {
+    response.text().then((text) => {
+      makeBlog(buildTweets(text));
+    });
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetchBlog();
+});
