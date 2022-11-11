10bdc8a82232224d1edd499adf99029d95b088dc
Natalie Martin <nmartin2473@gmail.com>
2022-11-11 13:47:38 -0700

    Minor: Add special date reasons to compiled Typescript
    Allow people who are curious to see what each special day means without
    having to visit the Github repo.

--- src/style/autostyle.ts
+++ src/style/autostyle.ts
@@ -1,57 +1,63 @@
+interface specialDate {
+  code: string;
+  reason: string;
+}
+
 function setBackground(hour?: number) {
   const date = new Date();
 
   const code = date.getMonth() + 1 + "/" + date.getDate();
   const specialDates = [
-    "1/1", // new year
-    "1/5", // shion day
-    "1/18", // mlk day
-    "1/21", // takane day
-    "1/26", // website birthday
-    "2/5", // dorothy + leona day
-    "2/18", // website relaunch birthday
-    "2/19", // legacy saturday
-    "2/29", // leap day
-    "3/3", // hinamatsuri
-    "3/17", // st. patrick's day
-    "3/21", // falulu day, spring
-    "3/29", // hardy fox day
-    "4/1", // april foolz
-    "4/13", // in between dreams :)
-    "4/19", // me birthday
-    "5/4", // ??? birthday
-    "5/6", // world accordion day
-    "6/6", // aroma day
-    "6/12", // konomi day
-    "6/21", // summer solstice
-    "7/4", // AX weekend day (lol)
-    "7/25", // ??? birthday
-    "7/30", // sophie day
-    "8/1", // mahjong day
-    "8/8", // mia day
-    "8/11", // fuwari day
-    "8/14", // bio3 prologue day
-    "8/21", // bio3 start day
-    "9/3", // aira + rizumu day
-    "9/13", // mata nui day
-    "9/21", // do you remember?
-    "10/1", // mirei day
-    "10/4", // ??? birthday
-    "10/30", // halloween eve
-    "10/31", // halloween
-    "11/11", // ii pocky day
-    "11/20", // lala day
-    "12/21", // winter solstice
-    "12/24", // christmas eve
-    "12/25", // christmas
-    "12/26", // boxing day
-    "12/31", // new yearz eve
+    { code: "1/1", reason: "new year" },
+    { code: "1/5", reason: "shion day" },
+    { code: "1/18", reason: "mlk day" },
+    { code: "1/21", reason: "takane day" },
+    { code: "1/26", reason: "classic website birthday" },
+    { code: "2/5", reason: "dorothy + leona day" },
+    { code: "2/18", reason: "website relaunch birthday" },
+    { code: "2/19", reason: "legacy saturday" },
+    { code: "2/29", reason: "leap day" },
+    { code: "3/3", reason: "hinamatsuri" },
+    { code: "3/17", reason: "st. patrick's day" },
+    { code: "3/21", reason: "falulu day, spring" },
+    { code: "3/29", reason: "hardy fox day" },
+    { code: "4/1", reason: "april foolz" },
+    { code: "4/13", reason: "in between dreams :)" },
+    { code: "4/19", reason: "me birthday" },
+    { code: "5/4", reason: "??? birthday" },
+    { code: "5/6", reason: "world accordion day" },
+    { code: "6/6", reason: "aroma day" },
+    { code: "6/12", reason: "konomi day" },
+    { code: "6/21", reason: "summer solstice" },
+    { code: "7/4", reason: "AX weekend day (lol)" },
+    { code: "7/25", reason: "??? birthday" },
+    { code: "7/30", reason: "sophie day" },
+    { code: "8/1", reason: "mahjong day" },
+    { code: "8/8", reason: "mia day" },
+    { code: "8/11", reason: "fuwari day" },
+    { code: "8/14", reason: "bio3 prologue day" },
+    { code: "8/21", reason: "bio3 start day" },
+    { code: "9/3", reason: "aira + rizumu day" },
+    { code: "9/13", reason: "mata nui day" },
+    { code: "9/21", reason: "do you remember?" },
+    { code: "10/1", reason: "mirei day" },
+    { code: "10/4", reason: "??? birthday" },
+    { code: "10/30", reason: "halloween eve" },
+    { code: "10/31", reason: "halloween" },
+    { code: "11/11", reason: "ii pocky day" },
+    { code: "11/20", reason: "lala day" },
+    { code: "12/21", reason: "winter solstice" },
+    { code: "12/24", reason: "christmas eve" },
+    { code: "12/25", reason: "christmas" },
+    { code: "12/26", reason: "boxing day" },
+    { code: "12/31", reason: "new yearz eve" },
   ];
-  if (specialDates.includes(code)) {
+  const codesList = specialDates.map((date) => date.code);
+  if (codesList.includes(code)) {
     (window as any)["specialDay"] = true;
     // Subtract 1900 from date to emulate date.getYear() for full Y2K aesthetic lol
     const offset =
-      date.getFullYear() - 1900 + specialDates.indexOf(code) + date.getDate();
+      date.getFullYear() - 1900 + codesList.indexOf(code) + date.getDate();
     const backgrounds = [
       "/style/backgrounds/checker.png",
       "/style/backgrounds/diamonds.png",
