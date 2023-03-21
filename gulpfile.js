c751ee9f799db290aa03e22b6149d74015bad599
Natalie Martin <nmartin2473@gmail.com>
2023-03-20 17:12:54 -0700

    Minor: Portfolio Work
    Adds gulp:watch, HTML template support, dotenv support. And the first
    draft of my portfolio.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
-var { src, dest, series } = require("gulp");
+var { src, dest, series, watch } = require("gulp");
 var browserify = require("gulp-browserify");
 var ts = require("gulp-typescript");
 var uglify = require("gulp-uglify");
 const babel = require("gulp-babel");
 var tsProject = ts.createProject("tsconfig.json");
 const sass = require("gulp-sass")(require("sass"));
+const replace = require("gulp-replace");
+const rename = require("gulp-rename");
+
+require("dotenv").config({ path: "src/portfolio/.env" });
 
 function megatranspile() {
   return src("src/**/*.ts")
@@ -34,5 +38,29 @@ function sassy() {
     .pipe(dest("src"));
 }
 
+function htmly() {
+  return src("src/**/*.html.template")
+    .pipe(
+      replace(/\{\{\s*(\w+?)\s*\}\}/g, (_match, key) => {
+        return process.env[key] ?? key;
+      })
+    )
+    .pipe(
+      rename((path) => {
+        path.basename = path.basename.replace(/.html$/, "");
+        path.extname = ".html";
+      })
+    )
+    .pipe(dest("src"));
+}
+
+function watchy() {
+  watch("src/**/*.scss", sassy);
+  watch("src/**/*.ts", megatranspile);
+  watch("src/**/*.html.template", htmly);
+}
+
 exports.sass = sassy;
-exports.default = series(megatranspile, sassy);
+exports.html = htmly;
+exports.default = series(htmly, megatranspile, sassy);
+exports.watch = watchy;
---
+++ src/portfolio/script.ts
@@ -0,0 +1,97 @@
+type Theme = "light" | "dark" | "muffins";
+let initialThemeSet = false;
+
+function getTheme(): Theme {
+  const currentTheme = getCurrentBodyTheme();
+  if (currentTheme) {
+    return currentTheme;
+  }
+  const darkPreference = localStorage.getItem("theme");
+  if (darkPreference) {
+    if (darkPreference.toLocaleLowerCase() === "dark") {
+      return "dark";
+    } else {
+      return "light";
+    }
+  }
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    return "dark";
+  }
+  return "light";
+}
+
+function getCurrentBodyTheme(): Theme | null {
+  if (location.search.toLocaleLowerCase().substring(1) === "muffins") {
+    return "muffins";
+  }
+  const classList = Array.from(document.body.classList).filter(
+    (name) => name === "light" || name === "dark" || name === "muffins"
+  );
+  const className = classList.pop() as Theme;
+  return className ?? null;
+}
+
+function saveTheme(theme: Theme): void {
+  localStorage.setItem("theme", theme);
+}
+
+function setTheme(theme: Theme): void {
+  document.body.className = theme;
+  if (initialThemeSet) {
+    document.body.classList.add("transition");
+  }
+  initialThemeSet = true;
+}
+
+function toggleTheme(): void {
+  const theme = getTheme();
+  const newTheme = theme === "dark" ? "light" : "dark";
+  setTheme(newTheme);
+  saveTheme(newTheme);
+
+  const themeToggleButtons = Array.from(
+    document.querySelectorAll(".theme-switcher")
+  );
+  for (const button of themeToggleButtons) {
+    const btn = button as HTMLElement;
+    btn.innerText = newTheme === "dark" ? "☀️" : "🌙";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const theme: Theme = getTheme();
+  setTheme(theme);
+  saveTheme(theme);
+
+  if (theme === "muffins") {
+    const description = document.querySelector(".about-me h2") as HTMLElement;
+    if (description) {
+      description.innerText = description.innerText.replace(/dev/i, "DERPv");
+      description.innerText = description.innerText.replace(
+        /software/i,
+        "MUFFIN"
+      );
+    }
+
+    const links = Array.from(document.querySelectorAll("a[href]"));
+    for (const linkNode of links) {
+      const link = linkNode as HTMLAnchorElement;
+      if (link.href.startsWith(location.protocol + "//" + location.host)) {
+        link.href = link.href + "?muffins";
+      }
+    }
+  }
+
+  const themeToggleButtons = Array.from(
+    document.querySelectorAll(".theme-switcher")
+  );
+  for (const button of themeToggleButtons) {
+    const btn = button as HTMLElement;
+    btn.innerText = theme === "dark" ? "☀️" : "🌙";
+    if (theme === "muffins") {
+      btn.style.display = "none";
+    } else {
+      btn.addEventListener("click", toggleTheme);
+    }
+  }
+});
