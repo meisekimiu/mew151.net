bf11092632f0fca58580db878972dda2b6b7fafe
Natalie Martin <nmartin2473@gmail.com>
2023-06-13 20:21:30 -0500

    Minor: Navigation e2e tests
    Check for parity between noscript / script navigation and make sure
    noscript nav is defined with target="_parent"

---
+++ test/nav.spec.ts
@@ -0,0 +1,51 @@
+import "expect-puppeteer";
+
+interface NavLink {
+  image: string | undefined;
+  link: string;
+}
+
+describe("Navigation", () => {
+  test("JS and Fallback Nav Parity", async () => {
+    await page.goto("http://127.0.0.1:8081");
+    const navLinks = await page.$eval("nav .navigation-group", (group) =>
+      Array.from(group.querySelectorAll("a")).map((element) => {
+        const obj: NavLink = {
+          image: element.querySelector("img")?.src,
+          link: element.href,
+        };
+        return obj;
+      })
+    );
+    await page.goto("http://127.0.0.1:8081/nav-fallback.html");
+    const fallbackLinks = await page.$eval(".navigation-group", (group) =>
+      Array.from(group.querySelectorAll("a")).map((element) => {
+        const obj: NavLink = {
+          image: element.querySelector("img")?.src,
+          link: element.href,
+        };
+        return obj;
+      })
+    );
+    for (const navLink of navLinks) {
+      expect(
+        fallbackLinks.find(
+          (link) => link.image === navLink.image && link.link === navLink.link
+        )
+      ).toBeTruthy();
+    }
+    expect(fallbackLinks.length).toBeGreaterThan(navLinks.length);
+  });
+  test("Fallback Nav opens in parent frame", async () => {
+    await page.goto("http://127.0.0.1:8081/nav-fallback.html");
+    const linkCount = await page.$eval(
+      ".navigation-group",
+      (group) => group.querySelectorAll("a").length
+    );
+    const linkParentCount = await page.$eval(
+      ".navigation-group",
+      (group) => group.querySelectorAll('a[target="_parent"]').length
+    );
+    expect(linkParentCount).toBe(linkCount);
+  });
+});
