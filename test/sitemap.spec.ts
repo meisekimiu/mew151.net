e15edc557950048c67370d152b3a459c7fb1a8ce
Natalie Martin <nmartin2473@gmail.com>
2023-06-14 19:54:59 -0500

    Minor: Add sitemap tests
    Add some more end-to-end tests that verify that all (meaningful) pages
    are linked to on the sitemap page, and also fix the sitemap to pass
    tests.

---
+++ test/sitemap.spec.ts
@@ -0,0 +1,38 @@
+import "expect-puppeteer";
+import { globSync } from "glob";
+
+const skipFiles = [
+  "src/not_found.html",
+  "src/nav-fallback.html",
+  "src/accordion/blog/posts/index.html",
+  "src/index.html",
+  /^src\/blog/,
+  /^src\/portfolio/,
+];
+
+const htmlFiles = globSync("src/**/*.html").filter(
+  (file) =>
+    !skipFiles.reduce((result, skip) => result || !!file.match(skip), false)
+);
+
+describe("Sitemap verification", () => {
+  let linkPaths: string[];
+  beforeAll(async () => {
+    await page.goto("http://127.0.0.1:8081/sitemap.html");
+    linkPaths = await page.$eval("main", (main) => {
+      return Array.from(main.querySelectorAll("a[href]")).map(
+        (element) => (element as HTMLAnchorElement).href
+      );
+    });
+  });
+  test.each(htmlFiles)("Sitemap: %s", async (file) => {
+    const expectedLinkPath = file
+      .replace(/^src\//, "")
+      .replace(/\/?index.html$/, "");
+    const containsLink = linkPaths.reduce(
+      (result, link) => result || link.endsWith(expectedLinkPath),
+      false
+    );
+    expect(containsLink).toBeTruthy();
+  });
+});
