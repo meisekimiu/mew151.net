6a63383f4459c5a98a0e62af151f6055f7ace9ad
Natalie Martin <nmartin2473@gmail.com>
2023-09-08 20:21:23 -0700

    Minor: Add skip link + skip link test
    Make site more accessible for keyboard users by adding skip link
    functionality to the site.

--- test/validity.spec.ts
+++ test/validity.spec.ts
@@ -25,4 +25,27 @@ describe("HTML Validity and Accessibility", () => {
     }
     expect(pageContents).toHTMLValidate();
   });
+  test.each(htmlFiles)("Main has id=main: %s", async (file) => {
+    const relativeFileName = file.replace(/^src\//, "");
+    await page.goto(getSiteUrl(relativeFileName));
+    const navClass = await page.$("nav.explorer");
+    if (navClass && !file.includes("nav-fallback.html")) {
+      const mainId = await page.$eval("main", (element) => {
+        return element.id;
+      });
+      expect(mainId).toBe("main");
+    }
+  });
+  test("Skip Link in nav", async () => {
+    await page.goto(getSiteUrl("/"));
+    const link = await page.$eval("nav a.skiplink", (element) => {
+      return {
+        tabindex: element.tabIndex,
+        href: element.href,
+      };
+    });
+    expect(link).toBeTruthy();
+    expect(link.tabindex).toBe(0);
+    expect(link.href).toContain("#main");
+  });
 });
