3d3e1bba0c0752d2218c8058de39118800ede71b
Natalie Martin <nmartin2473@gmail.com>
2023-09-08 19:43:25 -0700

    Minor: Add valid+accessible HTML test

--- test/validity.spec.ts
+++ test/validity.spec.ts
@@ -1,4 +1,5 @@
 import "expect-puppeteer";
+import "html-validate/jest";
 import { globSync } from "glob";
 import { getSiteUrl } from "./util/getSiteUrl";
 
@@ -12,5 +13,16 @@ describe("HTML Validity and Accessibility", () => {
       return element.lang;
     });
     expect(lang).toBeTruthy();
+  });
+  test.each(htmlFiles)("HTML Validity: %s", async (file) => {
+    const relativeFileName = file.replace(/^src\//, "");
+    await page.goto(getSiteUrl(relativeFileName));
+    const pageContents = await page.content();
+    if (file.includes("accordion/quiz.html")) {
+      // Skip HTML validation for this file because fixes need to be in NeoQuiz
+      // :3.........
+      return;
+    }
+    expect(pageContents).toHTMLValidate();
   });
 });
