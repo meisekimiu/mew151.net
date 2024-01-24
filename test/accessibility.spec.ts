657b4b10864c44e0cc55a8797d6c456bb35261f0
Natalie Martin <nmartin2473@gmail.com>
2024-01-23 22:45:50 -0700

    Minor: Update to latest version of NeoQuiz
    Also update tests now that NeoQuiz renders valid HTML.

---
+++ test/accessibility.spec.ts
@@ -0,0 +1,40 @@
+import "expect-puppeteer";
+import "html-validate/jest";
+import { globSync } from "glob";
+import { getSiteUrl } from "./util/getSiteUrl";
+
+const htmlFiles = globSync("src/**/*.html");
+
+describe("Accessibility", () => {
+  test.each(htmlFiles)("HTML Lang: %s", async (file) => {
+    const relativeFileName = file.replace(/^src\//, "");
+    await page.goto(getSiteUrl(relativeFileName));
+    const lang = await page.$eval("html", (element) => {
+      return element.lang;
+    });
+    expect(lang).toBeTruthy();
+  });
+  test.each(htmlFiles)("Main has id=main: %s", async (file) => {
+    const relativeFileName = file.replace(/^src\//, "");
+    await page.goto(getSiteUrl(relativeFileName));
+    const navClass = await page.$("nav.explorer");
+    if (navClass && !file.includes("nav-fallback.html")) {
+      const mainId = await page.$eval("main", (element) => {
+        return element.id;
+      });
+      expect(mainId).toBe("main");
+    }
+  });
+  test("Skip Link in nav", async () => {
+    await page.goto(getSiteUrl("/"));
+    const link = await page.$eval("nav a.skiplink", (element) => {
+      return {
+        tabindex: element.tabIndex,
+        href: element.href,
+      };
+    });
+    expect(link).toBeTruthy();
+    expect(link.tabindex).toBe(0);
+    expect(link.href).toContain("#main");
+  });
+});
--- test/validity.spec.ts
+++ test/validity.spec.ts
@@ -6,46 +6,10 @@ import { getSiteUrl } from "./util/getSiteUrl";
 const htmlFiles = globSync("src/**/*.html");
 
 describe("HTML Validity and Accessibility", () => {
-  test.each(htmlFiles)("HTML Lang: %s", async (file) => {
-    const relativeFileName = file.replace(/^src\//, "");
-    await page.goto(getSiteUrl(relativeFileName));
-    const lang = await page.$eval("html", (element) => {
-      return element.lang;
-    });
-    expect(lang).toBeTruthy();
-  });
   test.each(htmlFiles)("HTML Validity: %s", async (file) => {
     const relativeFileName = file.replace(/^src\//, "");
     await page.goto(getSiteUrl(relativeFileName));
     const pageContents = await page.content();
-    if (file.includes("accordion/quiz.html")) {
-      // Skip HTML validation for this file because fixes need to be in NeoQuiz
-      // :3.........
-      return;
-    }
     expect(pageContents).toHTMLValidate();
-  });
-  test.each(htmlFiles)("Main has id=main: %s", async (file) => {
-    const relativeFileName = file.replace(/^src\//, "");
-    await page.goto(getSiteUrl(relativeFileName));
-    const navClass = await page.$("nav.explorer");
-    if (navClass && !file.includes("nav-fallback.html")) {
-      const mainId = await page.$eval("main", (element) => {
-        return element.id;
-      });
-      expect(mainId).toBe("main");
-    }
-  });
-  test("Skip Link in nav", async () => {
-    await page.goto(getSiteUrl("/"));
-    const link = await page.$eval("nav a.skiplink", (element) => {
-      return {
-        tabindex: element.tabIndex,
-        href: element.href,
-      };
-    });
-    expect(link).toBeTruthy();
-    expect(link.tabindex).toBe(0);
-    expect(link.href).toContain("#main");
   });
 });
